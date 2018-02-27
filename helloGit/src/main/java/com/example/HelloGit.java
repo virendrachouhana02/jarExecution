4b145824943ad7463240cd9d984a0ae6724b7d26
virendra chouhan <virendra02chouhan@gmail.com>
2018-02-27 18:49:27 +0530

    First Commit

---
+++ helloGit/src/main/java/com/example/HelloGit.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class HelloGit {
+		
+		public static void main(String[] args){
+			
+			while(true)
+			{
+				System.out.println("Hello Git");
+				try{
+					Thread.sleep(1000);
+				}catch(Exception e){
+					System.out.println(e);
+				}
+			}
+		}
+
+	}
+
+
